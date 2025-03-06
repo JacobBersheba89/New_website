25e6ca1922c24fafe67b27af32aec34f9f845629
JacobP <jakubpawlas89@gmail.com>
2025-03-06 23:45:40 +0100

    Add files via upload

---
+++ send_mail.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "jakubpawlas89@gmail.com";
+    $subject = "Nová zpráva z rezervačního formuláře";
+    $headers = "From: " . $email . "\r\n" .
+               "Reply-To: " . $email . "\r\n" .
+               "Content-Type: text/plain; charset=UTF-8";
+
+    $body = "Jméno: $name\nEmail: $email\n\nZpráva:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Zpráva byla úspěšně odeslána.";
+    } else {
+        echo "Chyba při odesílání zprávy.";
+    }
+} else {
+    echo "Neplatný požadavek.";
+}
+?>
