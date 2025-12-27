977a194b4aef8051a02dd1bb513d2ce8556f49c8
JamesPeak89 <jakubpawlas89@gmail.com>
2025-12-27 01:44:19 +0100

    Add files via upload

--- masaze/script.js
+++ masaze/script.js
@@ -16,11 +16,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const revealOnScroll = () => {
         const windowHeight = window.innerHeight;
-
         reveals.forEach(el => {
             const elementTop = el.getBoundingClientRect().top;
             const revealPoint = 120; // kdy se má objevit
-
             if (elementTop < windowHeight - revealPoint) {
                 el.classList.add('active');
             }
@@ -29,4 +27,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', revealOnScroll);
     revealOnScroll(); // spustí se i hned po načtení
+
+    /* =========================
+       WhatsApp bar animace
+    ========================= */
+    const whatsappBar = document.querySelector('.whatsapp-bar');
+    if(whatsappBar){
+        setTimeout(() => {
+            whatsappBar.classList.add('show');
+        }, 500); // po 0,5s se vysune
+    }
 });
